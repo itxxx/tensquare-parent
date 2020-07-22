06fa536549f51268ce36c69ddfcd30317f2ceebd
青崖 <itxxx@foxmail.com>
2020-07-22 15:57:52 +0800

    day04

--- tensquare-spit/src/main/java/com/tensquare/spit/controller/SpitController.java
+++ tensquare-spit/src/main/java/com/tensquare/spit/controller/SpitController.java
@@ -5,6 +5,7 @@ import com.tensquare.spit.service.SpitService;
 import entity.Result;
 import entity.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -14,6 +15,9 @@ import org.springframework.web.bind.annotation.*;
 public class SpitController {
     @Autowired
     private SpitService spitService;
+    @Autowired
+    private RedisTemplate redisTemplate;
+
 
     @RequestMapping(method= RequestMethod.GET)
     public Result findAll(){
@@ -44,4 +48,31 @@ public class SpitController {
         return new Result(true, StatusCode.OK, "删除成功");
 
     }
+    @RequestMapping(value = "/comment/{parentid}/{page}/{size}",method =RequestMethod.GET)
+    public Result findByParentid(@PathVariable String parentid,@PathVariable int page,@PathVariable int size){
+        return new Result(true, StatusCode.OK, "查询成功",spitService.findByParentid(parentid,page,size));
+    }
+
+
+    /**
+     * 点赞
+     * @param id
+     * @return
+     */
+    @RequestMapping(value="/thumbup/{id}",method=RequestMethod.PUT)
+    public Result updateThumbup(@PathVariable String id){
+        //判断用户是否点过赞
+        String userid="2023";// 后边我们会修改为当前登陆的用户
+        if(redisTemplate.opsForValue().get("thumbup_"+userid+"_"+id)!=null){
+            return new Result(false,StatusCode.REPERROR,"你已经点过赞了");
+        }
+        spitService.updateThumbup(id);
+        redisTemplate.opsForValue().set( "thumbup_"+userid+"_"+ id,"1");
+        return new Result(true,StatusCode.OK,"点赞成功");
+
+    }
+
+
+
+
 }
--- tensquare-spit/src/main/java/com/tensquare/spit/service/SpitService.java
+++ tensquare-spit/src/main/java/com/tensquare/spit/service/SpitService.java
@@ -3,10 +3,18 @@ package com.tensquare.spit.service;
 import com.tensquare.spit.dao.SpitDao;
 import com.tensquare.spit.pojo.Spit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import uitl.IdWorker;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -16,6 +24,8 @@ public class SpitService {
     private IdWorker idWorker;
     @Autowired
     private SpitDao spitDao;
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     public List<Spit> findAll(){
        return spitDao.findAll();
@@ -39,4 +49,50 @@ public class SpitService {
     public void deleteById(String id){
         spitDao.deleteById(id);
     }
+
+    public Page<Spit> findByParentid(String parentid, int page,int size){
+        Pageable pageable= PageRequest.of(page,size);
+        return spitDao.findByParentid(parentid,pageable);
+    }
+
+
+
+    /**
+     * 点赞
+     * @param id
+     */
+    public void updateThumbup(String id){
+        /*Spit spit = spitDao.findById(id).get();
+        spit.setThumbup(spit.getThumbup()+1);
+        spitDao.save(spit);*/
+        Query query=new Query();
+        query.addCriteria(Criteria.where("_id").is(id));
+        Update update=new Update();
+        update.inc("thumbup",1);
+        mongoTemplate.updateFirst(query,update,"spit");
+    }
+
+    /**
+     * 发布吐槽（或吐槽评论）
+     * @param spit
+     */
+    public void add(Spit spit){
+        spit.set_id( idWorker.nextId()+"" );
+        spit.setPublishtime(new Date());//发布日期
+        spit.setVisits(0);//浏览量
+        spit.setShare(0);//分享数
+        spit.setThumbup(0);//点赞数
+        spit.setComment(0);//回复数
+        spit.setState("1");//状态
+        if(spit.getParentid()!=null && !"".equals(spit.getParentid())){// 如果存在上级ID,评论
+            Query query=new Query();
+            query.addCriteria(Criteria.where("_id").is(spit.getParentid()));
+            Update update=new Update();
+            update.inc("comment",1);
+            mongoTemplate.updateFirst(query,update,"spit");
+        }
+        spitDao.save(spit);
+    }
+
+
 }
